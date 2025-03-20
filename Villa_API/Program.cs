7684a5ed00cfcab9d5da1ac77cb78f83b3bcf47e
deepthiruby <deepthisreeruby@gmail.com>
2025-03-20 14:02:03 +0530

    replacing openapi with swashbuckle

--- Villa_API/Program.cs
+++ Villa_API/Program.cs
@@ -4,14 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi(); //ednpintapiexplorer and genand sui
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+   app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "VillaAPiEx");
+
+    });
+    //app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
